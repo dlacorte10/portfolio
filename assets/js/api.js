021f6dcef1dccaad86ad88dc2cc4f927064b975d
dlacorte10 <daniel.lacorte10@gmail.com>
2023-04-20 17:25:37 -0300

    Adding integration with json file

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,7 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/dlacorte10/portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/RenanJPaula/js-developer-portfolio/main/data/profile.json';
+                
     const fetching = await fetch(url)
     return await fetching.json()
 }
